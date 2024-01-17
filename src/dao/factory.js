75663732635f3f8a62f20a7260c1bd1f3eb9c7fb
bastianSalinas <bast.s.rojas@gmail.com>
2024-01-17 00:21:55 -0300

    Se actualiza persistence para que trabaje con railway

--- src/dao/factory.js
+++ src/dao/factory.js
@@ -6,7 +6,7 @@ export let Users
 export let Tickets
 switch (process.env.PERSISTENCE) {
     case "MONGO":
-        const connection = mongoose.connect(config.mongo_url)
+        const connection = mongoose.connect(process.env.MONGO_URL)
         const { default: CartsMongo } = await import('./mongo/carts.mongo.js')
         const { default: ProductsMongo } = await import('./mongo/products.mongo.js')
         const { default: UsersMongo } = await import('./mongo/users.mongo.js')
